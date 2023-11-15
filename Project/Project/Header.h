2de4e23461b2991baf8dd68af9a434bca0677ff8
MAXANDREIi <maximiucandrei22@stud.ase.ro>
2023-11-15 20:40:52 +0200

    Throwing exceptions

--- Project/Project/Header.h
+++ Project/Project/Header.h
@@ -10,22 +10,33 @@ private:
 
 public:
 	void setCityName(string cityName) {
+		if (cityName.size() <= 1)
+			throw exception("Name too short");
 		this->cityName = cityName;
 
 	}
 	string getCityName() {
 		return cityName;
 	}
-	void setVenueName(char* venueName) {
+	void setVenueName(const char* venueName) {
+		if (strlen(venueName) <= 1)
+			throw exception("Name too short");
 		delete[] this->venueName;
 		this->venueName = new char[strlen(venueName) + 1];
 		strcpy_s(this->venueName, strlen(venueName) + 1, venueName);
 
 	}
-	char* getVenueName() {
+	 char* getVenueName() {
 		char* copy = new char[strlen(this->venueName) + 1];
 		strcpy_s(copy, strlen(this->venueName) + 1, this->venueName);
 		return copy;
 	}
+
+};
+
+
+class Event {
+	string eventName = "";
+
 
 };
