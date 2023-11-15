6f57f318e6351c35ac2501b7d5ad56e9f832b13f
MAXANDREIi <maximiucandrei22@stud.ase.ro>
2023-11-15 20:27:40 +0200

    Testing Location class methods

--- Project/Project/FileName.cpp
+++ Project/Project/FileName.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 using namespace std;
+#include "Header.h"
 int main() {
-
-
-
+	Location location;
+	location.setCityName("Bucuresti");
+	location.setVenueName("Parcul Cismigiu");
+	cout<<location.getCityName();
+	cout << endl;
+	cout<<location.getVenueName();
 
 
 
