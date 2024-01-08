82709454bcada7c50792e3bbfefc2a06fc187c31
MAXANDREIi <maximiucandrei22@stud.ase.ro>
2024-01-08 19:14:09 +0200

    Reading from files

--- Project/Project/FileName.cpp
+++ Project/Project/FileName.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 #include "Location.h"
 #include "Event.h"
 #include "Ticket.h"
 int main() {
 	Ticket ticket;
-	cout << ticket.getTicketID()<<endl;
-	Ticket ticket2(200,TicketType::LAWN);
-	cout << ticket2.getTicketPrice()<<endl<<ticket2.getTicketType();
-	return 0;
+	ifstream data("data.txt");
+	data >> ticket;
+	cout << endl << ticket;
 }
+
+
