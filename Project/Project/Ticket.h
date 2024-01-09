c7d723e8d6ca9ebcf12a87e533e4c29f4a2e45aa
MAXANDREIi <maximiucandrei22@stud.ase.ro>
2024-01-10 01:18:54 +0200

    Making a seasonPass derived class

--- Project/Project/Ticket.h
+++ Project/Project/Ticket.h
@@ -175,3 +175,109 @@ void operator<<(ostream& console,  Ticket& ticket) {
 
 
 }
+
+enum class PassType{ONE_MONTH, HALF_YEAR, YEAR};
+class SeasonPass :public Ticket {
+private:
+    PassType passType = PassType::ONE_MONTH;
+    double price = 0.0;
+    char* passId = nullptr;
+    static char allPassIDS[500][9];
+    static int idIndex;
+
+    char* generatePassID() {
+        const char* digits = "0123456789";
+        const int idLenght = 8;
+        char* id = new char[idLenght + 1];
+        for (int i = 0; i < idLenght; i++) {
+            id[i] = digits[rand() % (sizeof(digits) - 1)];
+        }
+
+        id[idLenght] = '\0';
+        delete[] passId;
+        passId = new char[strlen(id) + 1];
+        strcpy_s(passId, strlen(id) + 1, id);
+        strcpy_s(allPassIDS[idIndex++], strlen(passId) + 1, passId);
+        return passId;
+    }
+
+
+public:
+  
+    double setPassPrice() {
+        switch (this->passType) {
+        case PassType::ONE_MONTH:
+            this->price = 249.99;
+            break;
+        case PassType::HALF_YEAR:
+            this->price = 1099.99;
+            break;
+        case PassType::YEAR:
+            this->price = 1799.99;
+            break;
+        default:
+            break;
+        }
+        return this->price;
+    }
+    
+    void getPassType() {
+        switch (this->passType)
+        {
+        case PassType::ONE_MONTH:
+            cout << "ONE MONTH";
+            break;
+        case PassType::HALF_YEAR:
+            cout << "HALF YEAR";
+            break;
+        case PassType::YEAR:
+            cout << "ONE YEAR";
+            break;
+        default:
+            break;
+        }
+   }
+
+    SeasonPass() : Ticket(), passType(PassType::ONE_MONTH) {}
+    SeasonPass(PassType passType):price(setPassPrice()){
+        this->passType = passType;
+    }
+    friend void operator<<(ostream& console, SeasonPass& pass);
+    friend void operator>>(istream& console, SeasonPass& pass);
+
+    void saveData(ostream& file) {
+        file.write((char*)&this->passType, sizeof(PassType));
+        file.write((char*)&this->price, sizeof(double));
+        int idSize = strlen(passId);
+        file.write((char*)&idSize, sizeof(int));
+        delete this->passId;
+        this->passId = new char[idSize];
+        file.write(this->passId, sizeof(char) * (idSize + 1));
+    }
+    int Valid(const char id[9]) {
+        int ok = 0;
+        for (int i = 0; i < idIndex; i++)
+            if (!strcmp(allPassIDS[i], id))
+                ok = 1;
+        return ok;
+    }
+
+};
+
+void operator>>(istream& console, SeasonPass& pass) {
+    cout << "Choose which type of pass do you want (0 for ONE month, 1 for SIX months and 2 for one year): ";
+    int input;
+    console >> input;
+    pass.passType = static_cast<PassType>(input);
+    pass.price = pass.setPassPrice();
+
+}
+
+void operator<<(ostream& console, SeasonPass& pass) {
+    console << endl << " Pass Type:"; pass.getPassType();
+    console << endl << " Pass Price: " << pass.price;
+    console << endl << " Pass ID: " << pass.getTicketID();
+}
+
+char SeasonPass::allPassIDS[500][9] = { " " };
+int SeasonPass::idIndex = 0;
